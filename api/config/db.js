806e15a4d71b810a1aca513627bde98dad9f99ee
kouame konan abraham <abrahamkonany@gmail.com>
2023-06-27 12:21:54 +0000

    ok

--- api/config/db.js
+++ api/config/db.js
@@ -1,19 +1,4 @@
-<<<<<<< HEAD
-import { connect } from "mongoose";
-import { InProduction } from "./env.js";
 
-export const connectDB =  async ()=>{
-
-    // eslint-disable-next-line no-undef
-    const MONGO_URI = process.env.MONGO_URI
-    if(!MONGO_URI) throw new Error("env variable : MONGO_URI is required" );
-    await connect(MONGO_URI, {
-        dbName: InProduction?'blog':'test_blog'
-    })
-}
-
-
-=======
 /* eslint-disable no-undef */
 import { connect } from 'mongoose';
 import { inProduction } from './env.js';
@@ -25,4 +10,4 @@ export const connectDB = async () => {
     dbName: inProduction ? 'blog' : 'test_blog',
   });
 };
->>>>>>> 30328a8c3fe039ada5ba02ca46ed8cc11a01c387
+
